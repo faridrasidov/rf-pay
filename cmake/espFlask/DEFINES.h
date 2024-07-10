aa5a322323988b69ba523062336928102e3ea094
FaridRasidov <ftm5pv70@duck.com>
2024-07-10 19:36:41 +0330

    add esp client-server with flask

---
+++ cmake/espFlask/DEFINES.h
@@ -0,0 +1,9 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+#define serverPort 51511
+
+const char* WIFI_SSID = "WIFI_SSID";
+const char* WIFI_PASSWORD = "WIFI_PASSWORD";
+const char* WIFI_ADDRESS = "SERVER_ADDRESS";
+
+#endif
